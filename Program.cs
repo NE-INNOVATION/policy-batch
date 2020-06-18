67d468b928089f22ff5cb92fb0d04ea4e6f51223
Afzal <mrafiq2@gmail.com>
2020-06-17 21:30:10 -0400

    Batch Job

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace batch_policy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Job Triggered!");
+
+            Thread.Sleep(20000);
+
+            if(!File.Exists("./test.txt")) File.Create("./test.txt");
+            
+            Console.WriteLine("Completed");
+
+
+        }
+    }
+}
