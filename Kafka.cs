7ce1749169a153b9d320ab6390e9e58298476f0e
Afzal <mrafiq2@gmail.com>
2020-06-19 01:48:36 -0400

    Kafka publisher

---
+++ Kafka.cs
@@ -0,0 +1,31 @@
+using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace batch_policy
+{
+    public class KafkaService
+    {
+        public static async Task<string> SendMessage(string policy, string bootstrapUrl = "my-cluster-kafka-bootstrap:9092")
+        {
+            var config = new ProducerConfig {
+                BootstrapServers = bootstrapUrl,
+            };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var dr = await p.ProduceAsync("policy", new Message<Null, string> { Value=policy });
+                    Console.WriteLine($"Delivered to topic '{dr.Topic}'");
+                    return $"Delivered to topic '{dr.Topic}'";
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+            return "success";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,43 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http;
 using System.Threading;
 
 namespace batch_policy
 {
     class Program
     {
+        static string url = Environment.GetEnvironmentVariable("POLICY-URL") ?? "http://istio-ingressgateway-istiosystem.apps.openshift.ne-innovation.com/policy/message";
+        static string bootstrap = Environment.GetEnvironmentVariable("BOOTSTRAP-SERVER") ?? "my-cluster-kafka-bootstrap:9092";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Job Triggered!");
+            try
+            {
+                Console.WriteLine("Job Triggered!");
+                Console.WriteLine("Argument lenght and value = " + args?.Length);
+                long timeToPoll = 0;
 
-            Thread.Sleep(20000);
+                if (!(args.Length > 0 && long.TryParse(args[0], out timeToPoll)))
+                {
+                    timeToPoll = 10000;
+                }
 
-            if(!File.Exists("./test.txt")) File.Create("./test.txt");
-            
-            Console.WriteLine("Completed");
-
+                var client = new HttpClient();
+                var result = client.GetAsync( url + "?time=" + timeToPoll).Result;
+                Console.WriteLine($"received message from kafka {result}");
+                
+                if (result.IsSuccessStatusCode)
+                {
+                    var response = result.Content.ReadAsStringAsync().Result;
+                    if (response != null && response != "[]") 
+                     KafkaService.SendMessage(response,bootstrap);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
         }
     }
