5e27326ce97b047b688c393d56c918913f25194c
Afzal <mrafiq2@gmail.com>
2020-06-20 01:52:39 -0400

    test

--- Program.cs
+++ Program.cs
@@ -10,29 +10,35 @@ namespace batch_policy
     {
         static string url = Environment.GetEnvironmentVariable("POLICY-URL") ?? "http://istio-ingressgateway-istiosystem.apps.openshift.ne-innovation.com/policy/message";
         static string bootstrap = Environment.GetEnvironmentVariable("BOOTSTRAP-SERVER") ?? "my-cluster-kafka-bootstrap:9092";
-        
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Job Triggered!");
+
             try
             {
-                Console.WriteLine("Job Triggered!");
-                Console.WriteLine($"Argument lenght and value ={args.FirstOrDefault()} " );
-                long timeToPoll = 0;
-
-                if (!(args.Length > 0 && long.TryParse(args[0], out timeToPoll)))
+                while (true)
                 {
-                    timeToPoll = 10000;
-                }
+                   
+                    Console.WriteLine($"Argument lenght and value ={args.FirstOrDefault()} ");
+                    long timeToPoll = 0;
+                    Thread.Sleep(1000);
 
-                var client = new HttpClient();
-                var result = client.GetAsync( url + "?time=" + timeToPoll).Result;
-                Console.WriteLine($"received message from kafka {result}");
+                    if (!(args.Length > 0 && long.TryParse(args[0], out timeToPoll)))
+                    {
+                        timeToPoll = 10000;
+                    }
 
-                if (result.IsSuccessStatusCode)
-                {
-                    var response = result.Content.ReadAsStringAsync().Result;
-                    if (response != null && response != "[]") 
-                     KafkaService.SendMessage(response,bootstrap);
+                    var client = new HttpClient();
+                    var result = client.GetAsync(url + "?time=" + timeToPoll).Result;
+                    Console.WriteLine($"received message from kafka {result}");
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var response = result.Content.ReadAsStringAsync().Result;
+                        if (response != null && response != "[]")
+                            KafkaService.SendMessage(response, bootstrap);
+                    }
                 }
             }
             catch (Exception ex)
